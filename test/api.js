b7957c61110367c5eefc31a6a1bbb0180ae76647
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-09-15 17:48:25 +0500

    I have no what I did in this, will fix later

--- test/api.js
+++ test/api.js
@@ -8,10 +8,11 @@ var url = "http://localhost:3000/";
 
 describe('Invoice API tests', function(){
   // level 1
-  it('Return "invoiceId", "amount", "wallet", "currency", and ' +
+  it('Should return "invoiceId", "amount", "wallet", "currency", and ' +
   '"rate" given "price", and "callback"', function(done){
     var bill = {
       price: 500,
+      callback: "http://test.payload.pk"
     };
     request.post(url + 'invoice', bill, function(err, head, body) {
       body = JSON.parse(body);
@@ -22,13 +23,14 @@ describe('Invoice API tests', function(){
       should.exist(body.amount);
       should.exist(body.address);
       should.exist(body.currency);
+      body.curency.should.equal('PKR');
       should.exist(body.rate);
       done();
     });
   });
 
   // level 2
-  it('Return "amount", "currency" and "rate" given "price"', function(){
+  it('Should get all invoices', function(){
 
   });
 });
